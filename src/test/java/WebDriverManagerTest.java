55eb39f37f853300352b94ec30a2d97888c844b2
zool <pdackow@wp.pl>
2022-12-14 08:23:40 +0100

    first commit

---
+++ src/test/java/WebDriverManagerTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class WebDriverManagerTest {
+
+    @Test
+    public void openBrowser() {
+//        WebDriverManager.chromedriver().setup();
+//        ChromeOptions options = new ChromeOptions();
+//        options.setBinary("D:\\programiki\\chrome pod selenium\\chrome-win\\chrome.exe");
+//        options.setHeadless(false);
+//        options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
+//        WebDriver driver = new ChromeDriver(options);
+//        JavascriptExecutor executor = (JavascriptExecutor) driver;
+//        executor.executeScript("alert('Hello')");
+//        driver.get("https://google.com");
+    }
+
+}
