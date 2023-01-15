dfb53773e6ab281e29a6bda40ad947356d07313c
zool <pdackow@wp.pl>
2023-01-16 00:22:52 +0100

    4th commit update

--- src/test/java/consolidationOfKnowledge.java
+++ src/test/java/consolidationOfKnowledge.java
@@ -12,13 +12,15 @@ public class consolidationOfKnowledge {
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
         driver.findElement(By.xpath("//*[@id=\"fname\"]")).sendKeys("ok");
-//        driver.findElement(By.cssSelector("#clickOnMe")).click();
-//        driver.findElement(By.cssSelector("body > select")).getAttribute("Saab");
+        driver.findElement(By.cssSelector("#clickOnMe")).click();
+        driver.findElement(By.cssSelector("body > select")).getAttribute("Saab");
         driver.findElement(By.cssSelector("body > form:nth-child(17) > input[type=text]:nth-child(2)")).clear();
         driver.findElement(By.cssSelector("body > form:nth-child(17) > input[type=text]:nth-child(2)")).sendKeys("juzek");
         driver.findElement(By.cssSelector("body > form:nth-child(17) > input[type=password]:nth-child(5)")).clear();
         driver.findElement(By.cssSelector("body > form:nth-child(17) > input[type=password]:nth-child(5)")).sendKeys("palka");
         driver.findElement(By.cssSelector("body > form:nth-child(17) > input[type=submit]:nth-child(8)")).click();
 
+        driver.findElement(By.linkText("IamWeirdLink")).click();
+        driver.findElement(By.partialLinkText("Iam")).click();
     }
 }
