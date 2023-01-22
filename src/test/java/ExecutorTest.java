75cb7e8a1b4dbc58bb4c34312030863f03a833d1
zool <pdackow@wp.pl>
2023-01-22 22:53:08 +0100

    commit NewWindowTest

---
+++ src/test/java/ExecutorTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class ExecutorTest {
+
+    @Test
+    public void executeJavascript() {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("D:\\programiki\\chrome pod selenium\\chrome-win\\chrome.exe");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://testeroprogramowania.github.io/selenium");
+
+        WebElement basicPageLink = driver.findElement(By.linkText("Podstawowa strona testowa"));
+//        basicPageLink.click();
+        // javascript code that allows you to write something on input
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();", basicPageLink);
+        WebElement firstName = driver.findElement(By.name("fname"));
+        executor.executeScript("arguments[0].setAttribute('value', 'ZooL');",firstName);
+
+
+    }
+
+}
