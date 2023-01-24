83431d0c0c081f00280c587c1cede32d4346e55c
zool <pdackow@wp.pl>
2023-01-24 23:34:52 +0100

    commit, delete some screenshots, add GetPageInfo

--- src/test/java/GetPageInfoTest.java
+++ src/test/java/GetPageInfoTest.java
@@ -16,6 +16,18 @@ public class GetPageInfoTest {
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
 
+
     }
 
 }
+//stworzyc test ktory zadziala na wszystkie elementy w basics
+//1. otworz przegladarke
+//2. pobiore tekst z headinga
+//3. klikam przycisk
+//4. klikam link
+//5. pobieram wartosc z tabelki np miesiac
+//6. Select wybieramy opcje
+//7. klikam potwierdzam 100%
+//8. wybieram opcje female
+//9. czyszcze pola nazwa/haslo klikam enter
+//10. klikam click me
---
+++ src/test/java/Homework.java
@@ -0,0 +1,57 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class Homework {
+// Things i want to do:
+//   Create tests that work on basics.html
+//1. open browser
+//2. get text from head
+//3. click bottom
+//4. click link
+//5. get text from array
+//6. Select Saab option
+//7. I submit option
+//8. i select "female"
+//9. clear input name/password than write new one
+//10. Click "click me"
+
+    @Test
+    public void homeWorkTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("D:\\programiki\\chrome pod selenium\\chrome-win\\chrome.exe");
+        WebDriver driver = new ChromeDriver(options);
+        //1
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        //2
+        System.out.println(driver.findElement(By.tagName("h1")).getText());
+        //3
+        driver.findElement(By.id("clickOnMe")).click();
+        driver.switchTo().alert().accept();
+        //4
+        driver.findElement(By.linkText("Visit W3Schools.com!")).click();
+        driver.findElement(By.id("accept-choices")).click();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        //5
+        System.out.println(driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[1]")).getText());
+        //6
+        driver.findElement(By.cssSelector("body > select > option:nth-child(2)")).click();
+        //7
+        driver.findElement(By.cssSelector("body > form:nth-child(16) > input[type=radio]:nth-child(3)")).click();
+        //8
+        driver.findElement(By.cssSelector("body > label:nth-child(15)")).click();
+        //9
+        driver.findElement(By.name("username")).clear();
+        driver.findElement(By.name("password")).clear();
+
+        driver.findElement(By.name("username")).sendKeys("ZooL");
+        driver.findElement(By.name("password")).sendKeys("Looking for job");
+
+        driver.findElement(By.id("newPage")).click();
+
+
+    }
+
+}
