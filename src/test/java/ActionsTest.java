2b695302f32bc053dd75117cc1b33f5210754ebf
zool <pdackow@wp.pl>
2023-01-24 21:00:45 +0100

    commit, UploadTest & ActionsTest + some png screenshots

--- src/test/java/ActionsTest.java
+++ src/test/java/ActionsTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
@@ -14,12 +15,17 @@ public class ActionsTest {
         ChromeOptions options = new ChromeOptions();
         options.setBinary("D:\\programiki\\chrome pod selenium\\chrome-win\\chrome.exe");
         WebDriver driver = new ChromeDriver(options);
-
-        driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
-
         //Using mouse right click~
+//        driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
+        //using mouse to double click
+        driver.get("https://testeroprogramowania.github.io/selenium/doubleclick.html");
         Actions actions = new Actions(driver);
-        // actions.contextClick().perform();
-        actions.contextClick(driver.findElement(By.id("myFile"))).perform();
+//         actions.contextClick().perform();
+//        actions.contextClick(driver.findElement(By.id("myFile"))).perform();
+
+        WebElement bottom = driver.findElement(By.id("bottom"));
+        actions.doubleClick(bottom).perform();
+
+
     } 
 }
