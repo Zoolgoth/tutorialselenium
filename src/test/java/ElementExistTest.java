cc6e4593e1b4500edf5b385f1c86ef4a1a157802
zool <pdackow@wp.pl>
2023-01-25 23:36:06 +0100

    ElementExistTest update

--- src/test/java/ElementExistTest.java
+++ src/test/java/ElementExistTest.java
@@ -1,39 +1,51 @@
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
-//import org.testng.annotations.Test;
-//
-//public class ElementExistTest {
-//
-//    WebDriver driver;
-//    ChromeOptions options = new ChromeOptions();
-//
-//
-//    @Test
-//    public void elementExistsTest() {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class ElementExistTest {
+
+
+    @Test
+    public void elementExistsTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("D:\\programiki\\chrome pod selenium\\chrome-win\\chrome.exe");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+
+        //checking if thing is displayed
+        System.out.println(driver.findElement(By.tagName("p")).isDisplayed());
+        System.out.println(driver.findElement(By.tagName("button")).isDisplayed());
+
+        //checking if thing is able to use
+        System.out.println(driver.findElement(By.tagName("button")).isEnabled());
+
+        //check if checkbox is clicked or not
+
+        System.out.println("-----");
+        WebElement checkbox = driver.findElement(By.cssSelector("[type='checkbox']"));
+        System.out.println(checkbox.isSelected());
+        checkbox.click();
+        System.out.println(checkbox.isSelected());
+
+
+
+    }
+
+//    public boolean elementExists (By locator) {
 //        driver = new ChromeDriver(options);
-//        options.setBinary("D:\\programiki\\chrome pod selenium\\chrome-win\\chrome.exe");
-//        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
-//        System.out.println(driver.findElement(By.tagName("p")));
-//        System.out.println(driver.findElement(By.id("topSecret")));
-//
-//        driver.findElement()
-//        if (driver.findElements(locator))
-//
+//        ChromeOptions options = new ChromeOptions();
+//        driver = new ChromeDriver(options);
+//        try {
+//            driver.findElement(locator);
+//            return true;
+//        } catch (NoSuchElementException ex) {
+//            return false;
+//        }
 //    }
-//
-////    public boolean elementExists (By locator) {
-////        driver = new ChromeDriver(options);
-////        ChromeOptions options = new ChromeOptions();
-////        driver = new ChromeDriver(options);
-////        try {
-////            driver.findElement(locator);
-////            return true;
-////        } catch (NoSuchElementException ex) {
-////            return false;
-////        }
-////    }
-//
-//
-//}
+
+
+}
