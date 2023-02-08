b44086f3d77942c42cdfec585abe55e7c941d5fe
zool <pdackow@wp.pl>
2023-02-08 23:01:05 +0100

    08.02 last commit

--- src/test/java/pl/testerOprogramowania/FourthTest.java
+++ src/test/java/pl/testerOprogramowania/FourthTest.java
@@ -1,6 +1,6 @@
 package pl.testerOprogramowania;
 
-import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class FourthTest {
@@ -19,10 +19,23 @@ public class FourthTest {
     @Test
     public void thirdTest() {
         System.out.println("I am third test");
-
-        String x = "I am third test";
+//        String x = "I am third test";
 //        Assert check real value, expected value
-        Assert.assertEquals(x, "I am third test");
+//        Assert.assertEquals(x, "I am third test");
+    }
+
+    @Test (dataProvider = "data")
+    public void dpTest(String val, String number) {
+        System.out.println(val);
+        System.out.println(number);
+
+
+    }
+
+        // Data Provider
+    @DataProvider(name = "data")
+    public Object[][] dataProvider() {
+        return new Object[][] {{"I am first test", "First"}, {"I am second test","Second"}, {"I am third test","Third"}};
 
     }
 
