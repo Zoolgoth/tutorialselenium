c588b8a744cb223bb7a359c1b0edfdb5fe126736
zool <pdackow@wp.pl>
2023-02-16 12:27:51 +0100

    First commit after Revert

---
+++ src/test/java/pl/travel/SignUpTest.java
@@ -0,0 +1,52 @@
+package pl.travel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pl.testerOprogramowania.DriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class SignUpTest {
+
+    // Testing http://www.kurs-selenium.pl/demo/
+    // filling the pots of city
+
+    @Test
+    public void singUp () throws InterruptedException {
+
+    WebDriver driver = DriverFactory.getDriver();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    driver.manage().window().maximize();
+    driver.get("http://www.kurs-selenium.pl/demo/");
+
+    driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream()
+          .filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+    driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+    // My solution:
+//    driver.findElement(By.name("firstname")).sendKeys("Juzek" + Keys.TAB + "Palka" +
+//            Keys.TAB + "48 500 699 676" + Keys.TAB + "pdackow@wp.pl" +
+//            Keys.TAB + "NewPassword123" + Keys.TAB + "NewPassword123");
+
+        String lastName = "Palka";
+        driver.findElement(By.name("firstname")).sendKeys("Juzek");
+        driver.findElement(By.name("lastname")).sendKeys(lastName);
+        driver.findElement(By.name("phone")).sendKeys("+48 500 699 676");
+        driver.findElement(By.name("email")).sendKeys("test10@test.pl");
+        driver.findElement(By.name("password")).sendKeys("NewPassword123");
+        driver.findElement(By.name("confirmpassword")).sendKeys("NewPassword123");
+
+        driver.findElement(By.xpath("//button[@type='submit' and text()=' Sign Up']")).click();
+
+       WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
+
+        Assert.assertTrue(heading.getText().contains(lastName));
+        Assert.assertEquals(heading.getText(), "Hi, Juzek Palka");
+
+
+    }
+
+}
